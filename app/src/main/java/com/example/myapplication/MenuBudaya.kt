ce0d429ae1d8602bf8967e7c39491a8c2c927170
Yusrian Asghany <yusrian2002@gmail.com>
2022-12-07 11:51:16 +0700

    Add MenuBudaya layout

---
+++ app/src/main/java/com/example/myapplication/MenuBudaya.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuBudaya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_budaya)
+    }
+}
