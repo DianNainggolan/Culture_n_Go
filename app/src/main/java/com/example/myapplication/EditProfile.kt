890d96620b2196fadc79cebf8db8318eca7c3b8e
DianNainggolan <93991097+DianNainggolan@users.noreply.github.com>
2022-12-09 16:40:59 +0700

    .

--- app/src/main/java/com/example/myapplication/EditProfile.kt
+++ app/src/main/java/com/example/myapplication/EditProfile.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityEditProfileBinding
 
 class EditProfile : AppCompatActivity() {
+
+    //viewbinding
+    private lateinit var binding: ActivityEditProfileBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
