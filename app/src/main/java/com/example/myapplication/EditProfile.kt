5a22441528437a4d281673c54c8dd3b644282f31
DianNainggolan <93991097+DianNainggolan@users.noreply.github.com>
2022-12-09 17:50:35 +0700

    .

--- app/src/main/java/com/example/myapplication/EditProfile.kt
+++ app/src/main/java/com/example/myapplication/EditProfile.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityEditProfileBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class EditProfile : AppCompatActivity() {
 
@@ -18,5 +22,35 @@ class EditProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        loadUserInfo()
+    }
+
+    private fun loadUserInfo() {
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref .child(firebaseAuth.uid!!)
+            .addValueEventListener(object: ValueEventListener {
+                //Get user info
+                override fun onDataChange(snapshot: DataSnapshot){
+                    val fotoprofile = "$(snapshot.child("fotoprofile").value)"
+                    val nama = "$(snapshot.child("nama").value)"
+                    val kontak = "$(snapshot.child("kontak").value)"
+                    val alamat = "$(snapshot.child("alamat").value)"
+
+                    //setdata
+                    binding.etName.setText(nama)
+                    binding.etContact.setText(kontak)
+                    binding.etAlamat.setText(alamat)
+
+                    //setImage
+                    
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+
     }
 }
