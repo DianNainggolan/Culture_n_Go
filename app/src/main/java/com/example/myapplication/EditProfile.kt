cce9c5abeb784c001f3788eba9029143d93b7cdd
DianNainggolan <93991097+DianNainggolan@users.noreply.github.com>
2022-12-09 20:56:46 +0700

    .

--- app/src/main/java/com/example/myapplication/EditProfile.kt
+++ app/src/main/java/com/example/myapplication/EditProfile.kt
@@ -1,16 +1,30 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.widget.PopupMenu
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityEditProfileBinding
+import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class EditProfile : AppCompatActivity() {
 
@@ -23,12 +37,21 @@ class EditProfile : AppCompatActivity() {
     //image uri(which we will pick)
     private var imageUri: Uri?= null
 
+    //progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //setup progres dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Mohon menunggu")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance()
         loadUserInfo()
 
@@ -44,8 +67,80 @@ class EditProfile : AppCompatActivity() {
 
         //tombol edit
         binding.btnEdit.setOnClickListener{
+            validateData()
+        }
+    }
+
+    private var name =""
+    private fun validateData() {
+        //get data
+        name = binding.etName.text.toString().trim()
+
+        //validate data
+        if(name.isEmpty()){
+            //kosong
+            Toast.makeText(this, "Masukkan nama", Toast.LENGTH_SHORT).show()
+        }else{
+            //name is entered
+            if(imageUri == null){
+                //need to update without image
+                updateProfile("")
+            }else{
+                //need to update with image
+                uploadImage()
+            }
 
         }
+    }
+
+    private fun uploadImage() {
+        progressDialog.setMessage("Mengupload gambar")
+        progressDialog.show()
+
+        //image phat and name, use uid to repale previous
+        val filePathAndName = "ProfileImage/"+firebaseAuth.uid
+
+        //storage reference
+        val reference = FirebaseStorage.getInstance().getReference(filePathAndName)
+        reference.putFile(imageUri!!)
+            .addOnSuccessListener {taskSnapShot->
+                // image uploaded, get url of uploaded image
+
+                val uriTask: Task<Uri> = taskSnapShot.storage.downloadUrl
+                while(!uriTask.isSuccessful);
+                val uploadedImageUrl = "${uriTask.result}"
+
+                updateProfile(uploadedImageUrl)
+            }
+            .addOnFailureListener{ e ->
+                progressDialog.dismiss()
+                Toast.makeText(this,"Gagal memasukkan gambar dikarenakan ${e.message}",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun updateProfile(uploadedImageUrl: String) {
+        progressDialog.setMessage("Mengupdate profil...")
+
+        //setup info to update to db
+        val hashmap: HashMap<String, Any> = HashMap()
+        hashmap["nama"] = "$name"
+        if(imageUri != null){
+            hashmap["fotoprofil"] = uploadedImageUrl
+        }
+
+        //update to db
+        val reference = FirebaseDatabase.getInstance().getReference("Users")
+        reference.child(firebaseAuth.uid!!)
+            .updateChildren(hashmap)
+            .addOnSuccessListener {
+                //profile updated
+                progressDialog.dismiss()
+                Toast.makeText(this,"Berhasil mengubah profil",Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{ e ->
+                progressDialog.dismiss()
+                Toast.makeText(this,"Gagal mengupdate profil dikarenakan ${e.message}",Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun loadUserInfo() {
@@ -54,7 +149,7 @@ class EditProfile : AppCompatActivity() {
             .addValueEventListener(object: ValueEventListener {
                 //Get user info
                 override fun onDataChange(snapshot: DataSnapshot){
-                    val fotoprofile = "${snapshot.child("fotoprofile").value}"
+                    val fotoprofil = "${snapshot.child("fotoprofil").value}"
                     val nama = "${snapshot.child("nama").value}"
                     val kontak = "${snapshot.child("kontak").value}"
                     val alamat = "${snapshot.child("alamat").value}"
@@ -67,7 +162,7 @@ class EditProfile : AppCompatActivity() {
                     //setImage
                     try{
                         Glide.with(this@EditProfile)
-                            .load(fotoprofile)
+                            .load(fotoprofil)
                             .placeholder(R.drawable.ic_person_gray)
                             .into(binding.tvProfile)
                     }catch (e:Exception){
@@ -111,10 +206,61 @@ class EditProfile : AppCompatActivity() {
     }
 
     private fun pickImageCamera(){
+        //intent to pick image from camera
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Temp_Title")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Temp_Description")
 
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        cameraActivityResultLauncher.launch(intent)
     }
 
     private fun pickImageGallery() {
-        TODO("Not yet implemented")
+        //intent to pick image from gallery
+        val intent= Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        galleryActivityResultLauncher.launch(intent)
     }
+
+    //used to handle result of camera intent(new way in replacement of startactivityforresutlt)
+    private val cameraActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback<ActivityResult>{result ->
+            //get url of image
+            if(result.resultCode == Activity.RESULT_OK){
+                val data = result.data
+                imageUri = data!!.data
+                //imageUri = data!!.data no need we already have image in imageUri in camera case
+
+
+                //set to imageview
+                binding.tvProfile.setImageURI(imageUri)
+            }else{
+                //cancelled
+                Toast.makeText(this,"Membatalkan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
+
+    //used to handle result of gallery intent(new way in replacement of startactivityforresutlt)
+    private val galleryActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback<ActivityResult>{ result ->
+            //get url of image
+            if(result.resultCode == Activity.RESULT_OK){
+                val data = result.data
+                imageUri = data!!.data
+
+                //set to imageview
+                binding.tvProfile.setImageURI(imageUri)
+            }else{
+                //cancelled
+                Toast.makeText(this,"Membatalkan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
+
 }
